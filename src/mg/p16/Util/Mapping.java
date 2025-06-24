fdf49c5c8c7cfcaa217decd67ada466e3c604f6e
tokyHerinjato <toukyhrj@gmail.com>
2025-06-24 16:04:29 +0300

    commit
    commit next

--- src/mg/p16/Util/Mapping.java
+++ src/mg/p16/Util/Mapping.java
@@ -1,20 +1,10 @@
 package mg.p16.Util;
 
-import java.lang.reflect.Method;
-import java.util.HashSet;
 import java.util.Set;
 
 public class Mapping {
-    String className;
-    String methodName;
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
+    private String className;
+    private Set<VerbAction> verbactions;
 
     public String getClassName() {
         return className;
@@ -29,28 +19,6 @@ public class Mapping {
 
     public Mapping(String className, String methodName) {
         setClassName(className);
-        setMethodName(methodName);
     }
 
-    // to execute the method inside the controllers annoted by a map
-    public Object invokeMethod() throws Exception {
-        Class<?> clazz = Class.forName(className);
-        Method method = clazz.getDeclaredMethod(methodName);
-        Object in_stance = clazz.getDeclaredConstructor().newInstance();
-        return method.invoke(in_stance);
-    }
-
-    public Set<VerbAction> getVerbactions() {
-        return verbactions;
-    }
-   
-    public void addVerbAction(String methodName, String verb) throws Exception {
-        // Vérifiez si le verbe est déjà utilisé
-        for (VerbAction action : verbactions) {
-            if (action.getVerb().equals(verb)) {
-                throw new Exception("HTTP verb " + verb + " already used by method: " + action.getMethodName());
-            }
-        }       
-        verbactions.add(new VerbAction(methodName, verb));
-    }
 }
